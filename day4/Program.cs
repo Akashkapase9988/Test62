c7ea652ddbfd2b903c1116de0c2d1ade223a227e
Akashkapase9988 <akashkapase9988@gmail.com>
2021-09-08 13:24:42 +0530

    day 4 UC1

---
+++ day4/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace day4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UC1_EmployeeAttendance.GetAttendance();
+            Console.ReadKey();
+        }
+    }
+}
