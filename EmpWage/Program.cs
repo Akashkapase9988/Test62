21433411a27f9d0d249b24a66347bf84c1a7c198
Akashkapase9988 <akashkapase9988@gmail.com>
2021-09-09 10:17:16 +0530

    practice problem EmpWage

---
+++ EmpWage/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace day4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //UC1_EmployeeAttendance.GetAttendance();
+            //UC2_EmpWage.EmpWage();
+            //UC3_PartTimeEmpWage.PartTimeEmpWage();
+            //UC4_EmpWageUsingSwitch.EmpWageUsingSwitch();
+            //UC5_CalculateWagesFor20DaysInMonth.CalculateWagesFor20DaysInMonth();
+            //UC6_CalculateWageTill100HrsOr20IsReached.CalculateWageTill100HrsOr20IsReached();
+            //UC7_ComputeEmpWageUsingClassMethod.ComputeEmpWage();
+            UC8_ComputeEmpWageForMultipleCompanies.ComputeEmpWage("Infosys",20,30,8);
+            Console.ReadKey();
+        }
+    }
+}
